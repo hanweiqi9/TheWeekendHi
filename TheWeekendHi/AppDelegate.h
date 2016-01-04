e8e3ad4d0e1311315ec27596e4fafee9b3ed9a04
韩苇棋 <hanweiqi@mail.com>
2016-01-04 11:23:21 +0800

    han

---
+++ TheWeekendHi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TheWeekendHi
+//
+//  Created by scjy on 16/1/4.
+//  Copyright © 2016年 芒果科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
